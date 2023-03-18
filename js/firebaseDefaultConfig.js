40d379fcf345a639f13d1f2bf60a3eadd3ac46f1
Huynh Duc Huy <duchuy.huynh.413@gmail.com>
2023-03-18 19:59:45 +0700

    update layout

--- js/firebaseDefaultConfig.js
+++ js/firebaseDefaultConfig.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBECKfjMFJ5Kml9FF1F2PVXGi8O1nsYAhs",
-    authDomain: "webdashboard-eecea.firebaseapp.com",
-    projectId: "webdashboard-eecea",
-    storageBucket: "webdashboard-eecea.appspot.com",
-    messagingSenderId: "347419719757",
-    appId: "1:347419719757:web:689dcd1c7cfb97bdeb8404",
-    measurementId: "G-B8CY71NLQE",
-    databaseURL: "webdashboard-eecea-default-rtdb.firebaseio.com"
-};
+    apiKey: "AIzaSyAm8rCcJgdE-2HW0UnR4rcmi4a8OhGUqZ4",
+    authDomain: "admin-board-23bae.firebaseapp.com",
+    databaseURL: "https://admin-board-23bae-default-rtdb.firebaseio.com",
+    projectId: "admin-board-23bae",
+    storageBucket: "admin-board-23bae.appspot.com",
+    messagingSenderId: "94548300181",
+    appId: "1:94548300181:web:623ea09099f64887a2e69c",
+    measurementId: "G-GM66T5J1E3"
+  };
 
 //Default conffig
 const app = firebase.initializeApp(firebaseConfig, 'main');
